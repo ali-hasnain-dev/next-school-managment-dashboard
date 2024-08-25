62a6ef95584d617f5621f256983e41c719017561
unknown <alihasnainhfd@gmail.com>
2024-08-25 15:02:10 +0500

    up

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -6,12 +6,12 @@ const TeacherListPage = () => {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* top */}
-      <div className="flex items-center justify-between">
+      <div className="flex justify-between items-center">
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:m-auto">
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
 
-          <div className="flex items-center gap-4 self-end">
+          <div className="flex items-center gap-4 self-end float-right">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" width={14} height={14} alt="filter" />
             </button>
--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const TableSearch = () => {
   return (
-    <div className=" w-full md:w-auto flex items-center gap-2 text-sm rounded-full ring-[1.5px] ring-gray-300 px-2">
-      <Image src="/search.png" width={14} height={14} alt="" />
+    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+      <Image src="/search.png" alt="" width={14} height={14} />
       <input
         type="text"
         placeholder="Search..."
